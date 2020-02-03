e383749028ffefe59c08db12bf7efd7c4fbd7255
Logan Frostick <frostilm@dukes.jmu.edu>
2020-02-02 23:20:28 -0500

    Update in branch

--- f1.c
+++ f1.c
@@ -4,3 +4,9 @@ main()
 	printf("and the presence of absence\n");
 	printf("Website not found!\n");
 }
+feature1()
+{
+	printf("No available space\n");
+	printf("Look to the cloud");
+	printf("Your drive is full!\n);
+}
